01da1beb55e7f9364d96835cb1903565707c07c1
Char-kun <panduhbyte@gmail.com>
2017-09-26 19:51:30 -0400

    Completed butrunning into bugs
    Currently testing if int variable can take a row array data. it does.

--- main.cpp
+++ main.cpp
@@ -39,17 +39,70 @@ int main(){
         cout<<endl;
     }
     cout<<endl;
+    int temp1;
+    temp1 = row[4][0];
+    cout<<temp1<<endl;
 
-    //##### Greedy algorithm and ppm creation.
+
+    /*
+    //##### Greedy algorithm
+    int  up1, up2, up3, mid1, mid2, mid3, dn1, dn2, dn3;
+    int now1, now2, now3, in1, in2, in3;
+    cout<<"Enter starting point 1.\n";
+    cin>>in1;
+    cout<<"Enter starting point 2.\n";
+    cin>>in2;
+    cout<<"Enter starting point 3.\n";
+    cin>>in3;
+
+    for(int nowCol=0; nowCol<width; nowCol++){
+        up1 = row[in1-1][nowCol+1];
+        up2 = row[in2-1][nowCol+1];
+        up3 = row[in3-1][nowCol+1];
+
+        mid1 = row[in1][nowCol+1];
+        mid2 = row[in2][nowCol+1];
+        mid3 = row[in3][nowCol+1];
+
+        dn1 = row[in1+1][nowCol+1];
+        dn2 = row[in2+1][nowCol+1];
+        dn3 = row[in3+1][nowCol+1];
+
+        if(in1>0 && up1<=mid1 && up1<=dn1){in1=up1;}
+        else if(in1<height && dn1<=mid1 && dn1<=up1){in1=dn1;}
+        else{in1=mid1;}
+        row[in1][nowCol]=maxVal;
+
+        if(in2>0 && up2<=mid2 && up2<=dn1){in2=up2;}
+        else if(in2<height && dn2<=mid2 && dn2<=up2){in2=dn2;}
+        else{in2=mid2;}
+        row[in2][nowCol]=maxVal;
+
+        if(in3>0 && up3<=mid3 && up3<=dn3){in3=up3;}
+        else if(in3<height && dn3<=mid3 && dn3<=up3){in3=dn3;}
+        else{in3=mid3;}
+        row[in3][nowCol]=maxVal;
+
+    }
+
+
+
+
+    // ppm creation.
     ofstream img;
     img.open("pic1.ppm");
     img << "P3\n" << width << " " << height << endl;
     img <<  maxVal << endl;
-    int  up1, up2, up3, mid1, mid2, mid3, dn1, dn2, dn3, now1, now2, now3;
-
-
+    for(int i=0; i<height; i++){
+        for(int j=0; j<width; j++){
+            if(row[i][j] == maxVal){img<<maxVal<<" 0 0\t";}
+            else{img<<row[i][j]<<" "<<row[i][j]<<" "<<row[i][j]<<"\t";}
+        }
+        img<<"\n";
+    }
     img.close();
     img.clear();
+*/
     // Deallocate 2D Array
     for(int i=0;i<height;i++){
         delete [] row[i];
@@ -62,7 +115,7 @@ int main(){
 void in_dat(){
     // ### Takes in file as string vector then convert to integer. ###
     ifstream infile;
-    infile.open("test2.dat");
+    infile.open("test1.dat");
     vector<string> numStr;
     vector<int> numVec;
 
