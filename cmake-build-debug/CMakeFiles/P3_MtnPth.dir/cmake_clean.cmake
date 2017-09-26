40f4927d387e9fbd19cf4c626144e31a20163fdb
Char-kun <panduhbyte@gmail.com>
2017-09-25 20:32:47 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/P3_MtnPth.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P3_MtnPth.dir/main.cpp.obj"
+  "P3_MtnPth.pdb"
+  "P3_MtnPth.exe"
+  "P3_MtnPth.exe.manifest"
+  "libP3_MtnPth.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P3_MtnPth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
